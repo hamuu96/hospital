460f8babb718cb11d23eeb91725869b51f8f8653
Hamza Abdikadir <hamanto96@gmail.com>
2021-12-05 21:40:42 +0000

    creation of delete appointment function

--- class/delete.class.php
+++ class/delete.class.php
@@ -30,6 +30,33 @@ class delete{
           }
 
     }
+    public function del_appointment($ap_id){
+      $sql = 'DELETE FROM  APPOINTMENT WHERE ap_id = ?';
+      $stmi = $this->connection->prepare($sql);
+      if ( false===$stmi ) {
+         
+          die('prepare() failed: ' . htmlspecialchars($this->connection->error));
+        }
+      $rc = $stmi->bind_param('i',$ap_id);
+      if ( false===$rc ) {
+          // again execute() is useless if you can't bind the parameters. Bail out somehow.
+          die('bind_param() failed: ' . htmlspecialchars($stmi->error));
+        }
+      $rc = $stmi->execute();
+      if ( false===$rc ) {
+          die('execute() failed: ' . htmlspecialchars($stmi->error));
+        }
+      // $result = $stmi->get_result();
+      // $dep_id = $result->fetch_all();
+
+     
+
+      // if (count($dep_id) > 0){
+      //     return $dep_id;
+      // }else{
+      //   return 'No appointments at the moments';
+      // }
+  }
 }
 
 
